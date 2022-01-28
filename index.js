5a1dd81a437217773e9a2ea84d4beefc9009e4ca
Tanya Rushing <tanyarushing@Tanyas-Air.lan>
2022-01-27 19:11:55 -0500

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let pageHeader = document.querySelector ("#page-header");
+pageHeader.style.backgroundColor = "black";
+pageHeader.style.color = "white";
 
-// Select some elements...
+let dogNames = document.querySelectorAll(".dog-name");
+for (let i = 0; i < dogNames.length; i++){
+    dogNames[i].style.color = "red";
+}
+let dogImages = document.querySelectorAll('.dog-image');
+for (let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = "15px"
+}
